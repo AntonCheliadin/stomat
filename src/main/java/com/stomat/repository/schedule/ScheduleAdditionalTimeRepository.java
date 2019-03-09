2930cd76d9ae7a7a0b61846fc4ff4d9d0ec1e763
antoncheliadin <chel_anton@mail.ru>
2019-03-09 09:51:04 +0200

    add schedule time repository

---
+++ src/main/java/com/stomat/repository/schedule/ScheduleAdditionalTimeRepository.java
@@ -0,0 +1,14 @@
+package com.stomat.repository.schedule;
+
+import com.stomat.domain.profile.Doctor;
+import com.stomat.domain.schedule.ScheduleAdditionalTime;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Collection;
+import java.util.Date;
+
+public interface ScheduleAdditionalTimeRepository extends CrudRepository<ScheduleAdditionalTime, Long> {
+
+
+    Collection<ScheduleAdditionalTime> findAllByDoctorEqualsAndFromDateAfterAndToDateBefore(Doctor doctor, Date from, Date to);
+}
