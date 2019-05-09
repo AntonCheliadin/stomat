a27f7ddb188e060172b101b090c58f1373f55e24
antoncheliadin <chel_anton@mail.ru>
2019-05-09 20:24:05 +0300

    doctor has managers (UserAccount) instead of own user

--- src/main/java/com/stomat/domain/profile/Doctor.java
+++ src/main/java/com/stomat/domain/profile/Doctor.java
@@ -1,10 +1,12 @@
 package com.stomat.domain.profile;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.stomat.domain.schedule.Schedule;
 import com.stomat.domain.schedule.ScheduleAdditionalTime;
 import com.stomat.domain.user.UserAccount;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Set;
 
 /**
@@ -18,10 +20,25 @@ public class Doctor {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank
+    private String firstName;
 
-    @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
-    private UserAccount user;
+    @NotBlank
+    private String fathersName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String email;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "doctor_manager",
+            joinColumns = {@JoinColumn(name = "doctor_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
+    )
+    private Set<UserAccount> managers;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "clinic_id")
@@ -41,12 +58,12 @@ public class Doctor {
         this.id = id;
     }
 
-    public UserAccount getUser() {
-        return user;
+    public Set<UserAccount> getManagers() {
+        return managers;
     }
 
-    public void setUser(UserAccount user) {
-        this.user = user;
+    public void setManagers(Set<UserAccount> managers) {
+        this.managers = managers;
     }
 
     public Clinic getClinic() {
@@ -71,5 +88,37 @@ public class Doctor {
 
     public void setAdditionalTimes(Set<ScheduleAdditionalTime> additionalTimes) {
         this.additionalTimes = additionalTimes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFathersName() {
+        return fathersName;
+    }
+
+    public void setFathersName(String fathersName) {
+        this.fathersName = fathersName;
     }
 }
